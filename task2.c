7fe3db7692fea3fc87da2c2a29cf21a7e4b8abd3
renu28choudhary <renu28choudhary@gmail.com>
2024-07-23 13:29:06 -0400

    new programs added

---
+++ task2.c
@@ -0,0 +1,12 @@
+//  Program to Print an Integer Entered By the User
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("enter integer: ");
+    scanf("%d", &n);
+    printf("%d", n);
+    return 0;
+}
