5c461fc284297cf209e522936d5afd4db76dc632
renu28choudhary <renu28choudhary@gmail.com>
2024-07-23 13:48:50 -0400

    task5Added

---
+++ task5.c
@@ -0,0 +1,11 @@
+// C Program to Find ASCII Value of a Character
+
+# include <stdio.h>
+
+int main() {
+    char c;
+    printf("Enter a character: ");
+    scanf("%c", &c);
+    printf("%c = %d", c, c);
+    return 0;
+}
