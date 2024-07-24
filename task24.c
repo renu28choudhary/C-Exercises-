3909a9719690ed5d02651a342008b596dde25922
renu28choudhary <renu28choudhary@gmail.com>
2024-07-24 13:38:24 -0400

    count number of digits program added

--- task24.c
+++ task24.c
@@ -23,3 +23,18 @@ int main()
 
     return 0;
 }
+
+// another way:
+
+// int main()
+// {
+//     int n = 3456778, count = 0;
+//     do
+//     {
+//         n = n / 10;
+//         count++;
+//     }
+//     while (n != 0);
+//     printf("%d", count);
+//     return 0;
+// }
