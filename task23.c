743f770a215cc00468aa5aee54ce7b75179c918a
renu28choudhary <renu28choudhary@gmail.com>
2024-07-24 11:07:03 -0400

    display alphabet program added

--- task23.c
+++ task23.c
@@ -1,1 +1,18 @@
-//
+// C Program to Display Characters from A to Z Using Loop
+
+#include <stdio.h>
+
+int main()
+{
+    char c;
+    for (c = 'a'; c <= 'z'; c++)
+    {
+        printf("%c ", c);
+    }
+    printf("\n");
+    for (c = 'A'; c <= 'Z'; c++)
+    {
+        printf(" %c ", c);
+    }
+    return 0;
+}
---
+++ task24.c
@@ -0,0 +1,7 @@
+// C Program to Count Number of Digits in an Integer:
+
+# include <stdio.h>
+
+int main() {
+    return 0;
+}
