20bf12da2697bbd45b0babf2ac3d5b6ef1bc3eb7
renu28choudhary <renu28choudhary@gmail.com>
2024-07-24 11:28:44 -0400

    number of digits count program added

--- task24.c
+++ task24.c
@@ -1,7 +1,25 @@
 // C Program to Count Number of Digits in an Integer:
 
-# include <stdio.h>
+#include <stdio.h>
 
-int main() {
+int main()
+{
+    int num, count = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    for (count = 1; count <= num; count++)
+    {
+        if (num > 0)
+        {
+            num = num / 10;
+            printf("%d number is\n ", num);
+            count = count++;
+            printf("count = %d \n ", count);
+        }
+    }
+    printf("%d final count ", count);
+
     return 0;
 }
