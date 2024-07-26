914637fc497b09a2581c2165fea0a25ab2a38ae9
renu28choudhary <renu28choudhary@gmail.com>
2024-07-26 14:52:31 -0400

    program added for factors

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,22 +1,2 @@
-#include <stdio.h>
-
-// int main()
-// {
-//     int oNum, remainder, result = 0, num; // 153
-
-//     printf("Enter a 3 digit number: ");
-//     scanf("%d", &oNum);
-//     num = oNum;
-//     while (num != 0)
-//     {
-//         remainder = num % 10;                                // 153 %10 //3
-//         result = result + remainder * remainder * remainder;
-//         //result = result + pow() //  3
-//         num = num / 10;                                      // 153/10 = 15
-//     }
-//     printf("%d result \n", result);
-//     printf("%d oNum", oNum);
-//     (result == oNum) ? printf("%d is Armstrong Number", oNum) : printf("%d is not an Armstrong number", oNum);
-
-//     return 0;
-// }
+printf("enter a number: ");
+    scanf("%d", &num);
