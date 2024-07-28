b108b3a8cf5b5268afc7799f765a89112f32dc78
renu28choudhary <renu28choudhary@gmail.com>
2024-07-27 21:38:35 -0400

    program for average of an array added

---
+++ Programiz Exercises/task45.c
@@ -0,0 +1,44 @@
+// C Program to Calculate Average Using Arrays
+
+#include <stdio.h>
+
+// int main()
+// {
+//     int myNumbers[] = {2, 3, 4, 5};
+//     int average;
+//     int sum = 0;
+//     int length = sizeof(myNumbers) / sizeof(myNumbers[0]);
+//     printf("length= %d\n", length);
+//     for (int i = 0; i <length; i++)
+//     {
+//         sum = sum + myNumbers[i];
+//     }
+//     printf("sum= %d \n", sum);
+//     average = sum / length;
+//     printf("average = %d \n", average);
+// }
+
+// for any array with user input
+
+#include <stdio.h>
+
+int main()
+{
+    int i, n;
+    float myArray[100], average, sum = 0.0;
+
+    printf("enter the numbe of elements in array: ");
+    scanf("%d", &n);
+    if (n <= 0)
+    {
+        printf("Error,please enter positive number!! \n");
+    }
+    for (i = 0; i < n; i++)
+    {
+        printf("enter myArray[%.1f]: ", i);
+        scanf("%f", &myArray[i]);
+        sum = sum + myArray[i];
+    }
+    average = sum / n;
+    printf("average= %.1f", average);
+}
