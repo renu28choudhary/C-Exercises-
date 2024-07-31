25e7790ac43e7556b3d49baa3c6d2c3bc429eb40
renu28choudhary <renu28choudhary@gmail.com>
2024-07-31 15:43:37 -0400

    program for minimum &maximum array added

--- GeeksForGeeksExercises/question13.c
+++ GeeksForGeeksExercises/question13.c
@@ -1,4 +1,6 @@
-#include <stdio.h>
+// Q13: Write a Program to find the maximum and minimum of an Array.
+
+// #include <stdio.h>
 
 // int main()
 // {
@@ -28,3 +30,30 @@
 //     printf("Maximum number: %d\n", maxNum);
 //     return 0;
 // }
+
+// user input array:
+
+#include <stdio.h>
+
+int main()
+{
+    int length, maxNum, minNum;
+    int myNum[length];
+    printf("Enter the length of array: ");
+    scanf("%d", &length);
+    for (int i = 0; i < length; i++)
+    {
+        printf("Enter the elements of array myNum[%d]:  ", i);
+        scanf("%d", &myNum[i]);
+    }
+    minNum = myNum[0];
+    for (int i = 1; i < length; i++)
+    {
+        if (minNum > myNum[i])
+        {
+            minNum = myNum[i];
+        }
+    }
+    printf("Minimum number: %d \n", minNum);
+    return 0;
+}
