1e3752eca1fe28a5093094b0f713a82c9ddeb759
renu28choudhary <renu28choudhary@gmail.com>
2024-08-13 15:03:24 -0400

    program sum of cube added

---
+++ w3ResourcePratice/w3Resource/hello/conditionalStatement/whileLoop/palindromeNumber.c
@@ -0,0 +1,6 @@
+// 9. Write a C program that implements a program to check if a given number is a palindrome using a while loop.
+
+#include<stdio.h>
+int main(){
+    return 0;
+}
--- w3ResourcePratice/w3Resource/hello/conditionalStatement/whileLoop/sumCube.c
+++ w3ResourcePratice/w3Resource/hello/conditionalStatement/whileLoop/sumCube.c
@@ -1,10 +1,31 @@
 // 8. Write a C program that calculates and prints the sum of cubes of even numbers up to a specified limit (e.g., 20) using a while loop.
 
-#include<stdio.h>
-#include<math.h>
+// #include<stdio.h>
+// #include<math.h>
 
-int main(){
-    int n, sum =0;
+// int main(){
+//     int n = 20, sum =0;
+//     for(int i=2;i<=n;i++){
+//         if(i%2 ==0){
+//             sum= sum + pow(i,3);
+//         }
+//     }
+//     printf("sum: %d", sum);
+//     return 0;
+// }
 
+// another way using while loop:
+
+#include <stdio.h>
+
+int main()
+{
+    int n = 20, sum = 0;
+    while (n > 0)
+    {
+        sum = sum + (n * n * n);
+        n = n - 2;
+    }
+    printf("sum: %d", sum);
     return 0;
 }
